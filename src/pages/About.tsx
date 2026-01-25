2eef0a38a5607bc06df6b95ede55a102886f183f
Wai <wai.yan@impact-h.co.jp>
2026-01-25 12:13:11 +0630

    feature/ui-update: scroll to Top in About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 //import AboutComponent from '../components/About';
 import AboutContact from '../components/AboutAndContact';
+import ScrollToTop from '../components/ScrollToTop';
 
 const About: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-50 pt-8">
+      <ScrollToTop />
       <AboutContact />
     </div>
   );
