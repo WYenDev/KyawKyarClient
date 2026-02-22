4bf3962791d1368bd1d1ce66133f0770406a5b41
Wai <wai.yan@impact-h.co.jp>
2026-02-22 23:31:43 +0630

    not run puppeteer

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { createRequire } from 'module';
-
-// ESM မှာ require ကို သုံးလို့ရအောင် ဖန်တီးခြင်း
-const require = createRequire(import.meta.url);
-const VitePrerender = require('vite-plugin-prerender');
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePrerender({
-      staticDir: path.join(__dirname, 'dist'),
-      routes: [
-        '/', 
-        '/buyCars', 
-        '/sellCars', 
-        '/about', 
-        '/payments', 
-        '/reviews', 
-        '/contact',
-        '/showroom-installment'
-      ],
-      renderer: new VitePrerender.PuppeteerRenderer({
-        renderAfterDocumentEvent: 'vite-prerender-ready',
-        headless: false,
-        renderAfterTime: 60000,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-      }),
-    }),
-  ],
+  plugins: [react()],
   server: {
     host: true,
   },
